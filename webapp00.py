55f65f54d94e48bae6b3c04f9e1f4c35a2efa59a
dani8799 <146739466+dani8799@users.noreply.github.com>
2023-10-02 11:17:07 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,17 +4,17 @@
 import streamlit as st
 
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("Prof. Massaki Igarashi")
+st.title("RESOLVE AÍ")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Teste de Cabeçalho")
+st.header("Um aplicativo para resolver seus problemas e promover o desenvolvimento profissional dos alunos!")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Bem vindos!")
+st.subheader("Bem vindo ao resolve aí!")
 
 # Use st.write("") para adicionar um texto ao seu Web app
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.write("Esse é um aplicativo onde as empresas podem coloca seus problemas para que os alunos resolvam.")
 
-st.info("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.info("Tem como objetivo trazer soluções "fora da caixa" para as empresas e ajudar os alunos a terem contato com o meio profissionl ainda dentro da universidade!")
 
 st.warning("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
